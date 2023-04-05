da8678bd196449490e974d6d78481d03e7f083db
Priscylla <pdar@ic.ufal.br>
2023-04-05 08:59:53 -0300

    fix question

--- Recursão, arrays, strings/transforma_decimal_para_binario.c
+++ Recursão, arrays, strings/transforma_decimal_para_binario.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int obtem_numero_binario(int numero) {
+void obtem_numero_binario(int numero) {
     
     int divisao = numero / 2; 
     numero = numero % 2; 
 
-    if(divisao == 0) return divisao;
+    if(divisao == 0) {
+        printf("%d\n", numero);
+        return;
+    }
     
     obtem_numero_binario(divisao);
 
@@ -16,6 +19,6 @@ int obtem_numero_binario(int numero) {
 int main() {
     int n;
     scanf("%d", &n);
-    printf("%d", obtem_numero_binario(n));
+    obtem_numero_binario(n);
     return 0;
 }
